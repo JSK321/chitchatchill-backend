6150616e22b4ea695de7f7fbff316c6848ccab22
Jae Kim <jaeshinkim321@gmail.com>
2021-03-09 12:12:42 -0800

    installed moment to reflect local time for createdat in messages

--- models/ChitChats.js
+++ models/ChitChats.js
@@ -1,9 +1,25 @@
+const moment = require('moment');
+
 module.exports = function (sequelize, DataTypes) {
     var ChitChats = sequelize.define("ChitChats", {
         message: DataTypes.TEXT,
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            get() {
+                return moment(this.getDataValue('createdAt')).format('lll');
+            }
+        },
+        updatedAt:{
+            type: DataTypes.DATE,
+            allowNull: false,
+            get() {
+                return moment(this.getDataValue('updatedAt')).format('lll');
+            }
+        }
     });
 
-    ChitChats.associate = function(models) {
+    ChitChats.associate = function (models) {
         ChitChats.belongsTo(models.ChatRooms);
         ChitChats.belongsTo(models.Users);
     };
